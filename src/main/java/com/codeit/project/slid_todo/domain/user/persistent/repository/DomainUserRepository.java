d017fd0c2a65ab6df44ef5cd7fa60625ce5e331b
ahj30420 <ahj30420@naver.com>
2025-07-08 17:41:52 +0900

    feat: userRepository 생성 및 조회,저장 기능 추가

---
+++ src/main/java/com/codeit/project/slid_todo/domain/user/persistent/repository/DomainUserRepository.java
@@ -0,0 +1,29 @@
+package com.codeit.project.slid_todo.domain.user.persistent.repository;
+
+import com.codeit.project.slid_todo.common.exception.BaseException;
+import com.codeit.project.slid_todo.domain.user.errorCode.UserErrorCode;
+import com.codeit.project.slid_todo.domain.user.persistent.entity.User;
+import com.codeit.project.slid_todo.domain.user.persistent.repository.jpaRepository.JpaUserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+
+@Slf4j
+@Repository
+@RequiredArgsConstructor
+public class DomainUserRepository {
+
+    private final JpaUserRepository jpaUserRepository;
+
+    public void assertEmailNotExists(String email) {
+        jpaUserRepository.findByEmail(email)
+                .ifPresent(user -> {
+                    throw new BaseException(UserErrorCode.EMAIL_ALREADY_EXISTS);
+                });
+    }
+
+    public void save(User user) {
+        jpaUserRepository.save(user);
+    }
+
+}
