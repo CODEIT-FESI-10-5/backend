af223bb02a86daeb7334ed01fa7ba6b02a402007
ahj30420 <ahj30420@naver.com>
2025-07-10 00:13:44 +0900

    feat: Spring Security 인증을 위한 UserDetailsService 구현

---
+++ src/main/java/com/codeit/project/slid_todo/domain/user/business/service/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package com.codeit.project.slid_todo.domain.user.business.service;
+
+import com.codeit.project.slid_todo.common.security.vo.CustomUserDetails;
+import com.codeit.project.slid_todo.domain.user.persistent.entity.User;
+import com.codeit.project.slid_todo.domain.user.persistent.repository.DomainUserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final DomainUserRepository domainUserRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User findUser = domainUserRepository.getByEmailOrThrow(username);
+        return CustomUserDetails.builder()
+                .id(findUser.getId())
+                .email(findUser.getEmail())
+                .password(findUser.getPassword())
+                .userRole(findUser.getUserRole())
+                .build();
+    }
+}
