7a56e8156fb83b547718c067029598f165829169
ahj30420 <ahj30420@naver.com>
2025-07-10 00:18:57 +0900

    feat: 로그인 실패 CustomAuthenticationFailureHandler 구현

---
+++ src/main/java/com/codeit/project/slid_todo/common/security/handler/CustomAuthenticationFailureHandler.java
@@ -0,0 +1,33 @@
+package com.codeit.project.slid_todo.common.security.handler;
+
+import com.codeit.project.slid_todo.common.dto.ResponseDto;
+import com.codeit.project.slid_todo.common.security.errorCode.AuthErrorCode;
+import com.codeit.project.slid_todo.common.util.ResponseUtil;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class CustomAuthenticationFailureHandler implements AuthenticationFailureHandler {
+
+    private final ResponseUtil responseUtil;
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        ResponseDto<Void> responseDto = ResponseDto.<Void>builder()
+                .httpStatusCode(HttpServletResponse.SC_OK)
+                .errorCode(AuthErrorCode.AUTHENTICATION_FAILED.getCode())
+                .errorMessage(AuthErrorCode.AUTHENTICATION_FAILED.getMessage())
+                .build();
+
+        responseUtil.writeJsonResponse(response, responseDto);
+    }
+
+}
