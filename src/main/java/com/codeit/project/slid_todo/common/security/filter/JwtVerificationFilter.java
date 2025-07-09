02f085a0322382bbc9e07ca038bc60dec54545aa
ahj30420 <ahj30420@naver.com>
2025-07-10 00:15:29 +0900

    feat: JWT 검증 필터(JwtVerificationFilter) 구현 및 예외 처리 추가

---
+++ src/main/java/com/codeit/project/slid_todo/common/security/filter/JwtVerificationFilter.java
@@ -0,0 +1,72 @@
+package com.codeit.project.slid_todo.common.security.filter;
+
+import com.codeit.project.slid_todo.common.security.errorCode.AuthErrorCode;
+import com.codeit.project.slid_todo.common.security.jwt.JwtProperties;
+import com.codeit.project.slid_todo.common.security.jwt.JwtProvider;
+import com.codeit.project.slid_todo.common.security.vo.CustomUserDetails;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.security.SignatureException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Slf4j
+@RequiredArgsConstructor
+public class JwtVerificationFilter extends OncePerRequestFilter {
+
+    private final JwtProvider jwtProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            setAuthenticationToContext(request);
+        } catch (ExpiredJwtException ee) {
+            request.setAttribute("errorCode", AuthErrorCode.ACCESS_TOKEN_EXPIRED);
+        } catch (SignatureException se) {
+            request.setAttribute("errorCode", AuthErrorCode.INVALID_SIGNATURE_ACCESS_TOKEN);
+        } catch (JwtException je) {
+            request.setAttribute("errorCode", AuthErrorCode.UNAUTHENTICATED);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) {
+        return !validAuthorizationHeader(request);
+    }
+
+    private boolean validAuthorizationHeader(HttpServletRequest request) {
+        String authorizationHeader = getAuthenticationTokenToHeader(request);
+        return authorizationHeader != null && authorizationHeader.startsWith(JwtProperties.TOKEN_PREFIX);
+    }
+
+    private void setAuthenticationToContext(HttpServletRequest request) {
+        SecurityContextHolder.getContext().setAuthentication(createAuthenticatedToken(request));
+    }
+
+    private Authentication createAuthenticatedToken(HttpServletRequest request) {
+        CustomUserDetails userDetails = createUserDetails(request);
+        return new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+    }
+
+    private CustomUserDetails createUserDetails(HttpServletRequest request) {
+        String token = getAuthenticationTokenToHeader(request).substring(JwtProperties.TOKEN_PREFIX.length());
+        return new CustomUserDetails(jwtProvider.getClaims(token));
+    }
+
+    private String getAuthenticationTokenToHeader(HttpServletRequest request) {
+        return request.getHeader(HttpHeaders.AUTHORIZATION);
+    }
+
+}
