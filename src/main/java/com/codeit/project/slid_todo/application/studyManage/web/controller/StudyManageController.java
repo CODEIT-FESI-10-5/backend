7b509af5d9ac5c1212f58903da6e7593be68179c
ahj30420 <ahj30420@naver.com>
2025-07-13 22:08:30 +0900

    feat: 스터디 목표 생성 기능 기본 구조 및 @CheckStudyLeader 적용

--- src/main/java/com/codeit/project/slid_todo/application/studyManage/web/controller/StudyManageController.java
+++ src/main/java/com/codeit/project/slid_todo/application/studyManage/web/controller/StudyManageController.java
@@ -1,11 +1,13 @@
 package com.codeit.project.slid_todo.application.studyManage.web.controller;
 
 import com.codeit.project.slid_todo.application.studyManage.business.service.StudyManageFacade;
+import com.codeit.project.slid_todo.common.annotation.CheckStudyLeader;
 import com.codeit.project.slid_todo.common.annotation.CurrentUser;
 import com.codeit.project.slid_todo.common.dto.ResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +21,17 @@ public class StudyManageController {
     public ResponseEntity<ResponseDto<Void>> createStudy( @CurrentUser Long userId ) {
 
         studyManageFacade.createStudy(userId);
+
+        ResponseDto<Void> response = ResponseDto.<Void>builder()
+                .httpStatusCode(HttpStatus.CREATED.value())
+                .build();
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @CheckStudyLeader
+    @PostMapping("/api/study/{studyId}")
+    public ResponseEntity<ResponseDto<Void>> createStudyGoal( @PathVariable Long studyId, @CurrentUser Long userId ) {
 
         ResponseDto<Void> response = ResponseDto.<Void>builder()
                 .httpStatusCode(HttpStatus.CREATED.value())
