18f78d80c9044526eadf2887559c91ed9fbe53dd
ahj30420 <ahj30420@naver.com>
2025-07-14 19:23:02 +0900

    feat: 스터디 수정 Controller 기능 추가

--- src/main/java/com/codeit/project/slid_todo/application/studyManage/web/controller/StudyManageController.java
+++ src/main/java/com/codeit/project/slid_todo/application/studyManage/web/controller/StudyManageController.java
@@ -1,15 +1,16 @@
 package com.codeit.project.slid_todo.application.studyManage.web.controller;
 
 import com.codeit.project.slid_todo.application.studyManage.business.service.StudyManageFacade;
+import com.codeit.project.slid_todo.application.studyManage.web.dto.EditStudyDto;
 import com.codeit.project.slid_todo.common.annotation.CheckStudyLeader;
 import com.codeit.project.slid_todo.common.annotation.CurrentUser;
 import com.codeit.project.slid_todo.common.dto.ResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
 
 @RestController
 @RequiredArgsConstructor
@@ -27,6 +28,22 @@ public class StudyManageController {
                 .build();
 
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @CheckStudyLeader
+    @PatchMapping("/api/study/{studyId}")
+    public ResponseEntity<ResponseDto<Void>> editStudy(
+            @ModelAttribute EditStudyDto.Request editStudyDto,
+            @PathVariable Long studyId,
+            @CurrentUser Long userId ) throws IOException {
+
+        studyManageFacade.updateStudy(editStudyDto, studyId);
+
+        ResponseDto<Void> response = ResponseDto.<Void>builder()
+                .httpStatusCode(HttpStatus.OK.value())
+                .build();
+
+        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
     @CheckStudyLeader
