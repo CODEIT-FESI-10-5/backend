8955961fdaa7039301931a4d0f953b977c2886b8
ahj30420 <ahj30420@naver.com>
2025-07-16 19:20:38 +0900

    feat: CodeGenerator 이름 변경

--- src/main/java/com/codeit/project/slid_todo/common/util/CodeGenerator.java
+++ src/main/java/com/codeit/project/slid_todo/common/util/CodeGenerator.java
@@ -2,14 +2,13 @@ package com.codeit.project.slid_todo.common.util;
 
 import java.security.SecureRandom;
 
-public class InviteCodeGenerator {
+public class CodeGenerator {
     private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    private static final int CODE_LENGTH = 8;
     private static final SecureRandom RANDOM = new SecureRandom();
 
-    public static String generate() {
-        StringBuilder sb = new StringBuilder(CODE_LENGTH);
-        for (int i = 0; i < CODE_LENGTH; i++) {
+    public static String generate(int length) {
+        StringBuilder sb = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
             int index = RANDOM.nextInt(CHARACTERS.length());
             sb.append(CHARACTERS.charAt(index));
         }
