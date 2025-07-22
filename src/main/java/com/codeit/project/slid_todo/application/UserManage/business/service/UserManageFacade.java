17e3aea890827e6135bb376ea1582db562ecad37
ahj30420 <ahj30420@naver.com>
2025-07-22 20:44:39 +0900

    feat: 프로필 수정 기능에 비밀번호 변경 추가

--- src/main/java/com/codeit/project/slid_todo/application/UserManage/business/service/UserManageFacade.java
+++ src/main/java/com/codeit/project/slid_todo/application/UserManage/business/service/UserManageFacade.java
@@ -7,13 +7,11 @@ import com.codeit.project.slid_todo.common.vo.UploadImg;
 import com.codeit.project.slid_todo.domain.user.business.service.UserService;
 import com.codeit.project.slid_todo.domain.user.persistent.entity.User;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.IOException;
 
-@Slf4j
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
@@ -23,26 +21,31 @@ public class UserManageFacade {
     private final ImgStore imgStore;
 
     @Transactional
-    public void editProfile(EditProfileDto.Request editProfileDto, Long userId) throws IOException {
+    public void editProfile(EditProfileDto.Request dto, Long userId) throws IOException {
         User user = userService.findUserById(userId);
-        String newNickname = editProfileDto.nickname();
-        UploadImg newImage = null;
 
-        if(shouldDeleteImg(editProfileDto.profileImageAction(), user)) {
+        UploadImg newImage = handleProfileImageUpdate(dto, user);
+
+        user.updateProfile(dto.nickname(), newImage);
+
+        if (dto.currentPassword() != null && dto.newPassword() != null) {
+            userService.changePassword(user, dto.currentPassword(), dto.newPassword());
+        }
+    }
+
+    private UploadImg handleProfileImageUpdate(EditProfileDto.Request dto, User user) throws IOException {
+        ProfileImageUpdateAction action = dto.profileImageAction();
+
+        if ((action == ProfileImageUpdateAction.RESET || action == ProfileImageUpdateAction.UPLOAD)
+                && user.hasCustomImage()) {
             imgStore.deleteImage(user.getImg().getStoreImgDir());
         }
 
-        log.info("editProfileDto.action={}", editProfileDto.profileImageAction());
-
-        newImage = switch (editProfileDto.profileImageAction()) {
-            case UPLOAD -> imgStore.storeImg(editProfileDto.newImageFile());
+        return switch (action) {
+            case UPLOAD -> imgStore.storeImg(dto.newImageFile());
             case RESET -> null;
             case NONE -> user.getImg();
         };
-
-        log.info("newImage={}", newImage);
-
-        user.updateProfile(newNickname, newImage);
     }
 
     private boolean shouldDeleteImg(ProfileImageUpdateAction action, User user) {
