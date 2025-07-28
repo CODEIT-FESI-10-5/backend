d11ab044b9b82c19263912c1c964059ec6a827fd
ahj30420 <ahj30420@naver.com>
2025-07-28 17:08:37 +0900

    feat: 불필요한 테스트 및 config 주석 처리

--- src/test/java/com/codeit/project/slid_todo/TestApplication.java
+++ src/test/java/com/codeit/project/slid_todo/TestApplication.java
@@ -1,12 +1,12 @@
-package com.codeit.project.slid_todo;
-
-import org.springframework.boot.SpringApplication;
-
-public class TestApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.from(Application::main)
-            .run(args);
-    }
-
-}
+//package com.codeit.project.slid_todo;
+//
+//import org.springframework.boot.SpringApplication;
+//
+//public class TestApplication {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.from(Application::main)
+//            .run(args);
+//    }
+//
+//}
